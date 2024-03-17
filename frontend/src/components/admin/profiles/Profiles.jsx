7ad6a9b514353288fda4c5f672e5ba2a55528f17
katarinasotic <64659755+katarinasotic@users.noreply.github.com>
2024-03-17 18:34:04 -0400

    adding 2 separated tables to profiles for patient and doctor

--- frontend/src/components/admin/profiles/Profiles.jsx
+++ frontend/src/components/admin/profiles/Profiles.jsx
@@ -9,110 +9,131 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab'; 
+
 const Profiles = () => {
+    const [prescriberPage, setPrescriberPage] = React.useState(0);
+    const [prescriberRowsPerPage, setPrescriberRowsPerPage] = React.useState(5); 
 
-    const [rowsPerPage, setRowsPerPage] = React.useState(5); 
-    const [page, setPage] = React.useState(0);
+    const [patientPage, setPatientPage] = React.useState(0);
+    const [patientRowsPerPage, setPatientRowsPerPage] = React.useState(5);
 
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
+    const [selectedTab, setSelectedTab] = React.useState(0); // State for selected tab
 
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(+event.target.value);
-        setPage(0); // Reset page to 0 when rows per page change
-    };
-
-    const sampleData = [
+    // Separate Data for Prescribers and Patients
+    const prescriberData = [ 
         { 
             id: 1,
             code: 'MD1001',
             first: 'John',
             last: 'Doe',
-            email: 'john.doe@example.com',
-            street: '123 Main St',
-            city: 'Toronto',
-            province: 'ON',
-            lang: 'EN',
-            college: 'CPSO', 
-            profession: 'Physician',
-            licence: 'A12345',
-            'view-prescription': true 
         },
+    ];
+
+    const patientData = [
         { 
-            id: 2,
-            code: 'RN2002',
+            id: 1,
+            code: 'PT3001', 
             first: 'Jane',
             last: 'Smith',
-            email: 'jane.smith@example.com',
-            street: '456 Elm Ave',
-            city: 'Ottawa',
-            province: 'ON',
-            lang: 'FR',
-            college: 'CNO', 
-            profession: 'Nurse',
-            licence: 'B67890',
-            'view-prescription': false
         },
-        // Add more entries as needed... 
     ];
-    
-    const columns = [
+
+    const columns = [ // Sample columns - add depending on what api returns
         { id: 'code', label: 'Code' },
         { id: 'first', label: 'First Name' },
         { id: 'last', label: 'Last Name' },
-        { id: 'email', label: 'Email' },
-        { id: 'street', label: 'Street' },
-        { id: 'city', label: 'City' },
-        { id: 'province', label: 'Province' },
-        { id: 'lang', label: 'Language' },
-        { id: 'college', label: 'Licencing College' },
-        { id: 'profession', label: 'Profession' },
-        { id: 'licence', label: 'Licence No' },
-        { id: 'view-prescription', label: '' },
     ];
 
+    const handlePrescriberPageChange = (event, newPage) => {
+        setPrescriberPage(newPage);
+    };
+
+    const handlePrescriberRowsPerPageChange = (event) => {
+        setPrescriberRowsPerPage(+event.target.value);
+        setPrescriberPage(0); 
+    };
+
+    const handlePatientPageChange = (event, newPage) => {
+        setPatientPage(newPage);
+    };
+
+    const handlePatientRowsPerPageChange = (event) => {
+        setPatientRowsPerPage(+event.target.value);
+        setPatientPage(0); 
+    };
+
+    const handleTabChange = (event, newValue) => {
+        setSelectedTab(newValue);
+    };
+
     return (
-        <div className='admin-verification-component'>
-        <div className='verif-header'> Your CSVs </div>
-        <div className='content'>
-            <div className='controls'>
-                <Button className='upload-button'>
-                    <span>Upload CSV</span>
-                </Button>
+        <div className='admin-profiles-component'>
+            <div className='verif-header'>
+                <Tabs value={selectedTab} onChange={handleTabChange}>
+                    <Tab className={selectedTab === 0 ? 'active tab-label' : 'tab-label'} label="Prescribers" />
+                    <Tab className={selectedTab === 1 ? 'active tab-label' : 'tab-label'} label="Patients" />
+                </Tabs>
             </div>
-            <div className='table-container'>
-                <TableContainer className='table-cont'>
-                    <Table className='table'>
-                        <TableHead className='table-header'>
-                            <TableRow className='header-row'>
-                                {columns.map((column) => (
-                                <TableCell key={column.id}>{column.label}</TableCell>
-                                ))}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {sampleData.map((row) => (
-                                <TableRow className='table-row' key={row.id}>
+            <div className='content'>
+                <div className='table-container'>
+                    <TableContainer className='table-cont'>
+                        <Table className='table'>
+                            <TableHead className='table-header'>
+                                <TableRow className='header-row'>
                                     {columns.map((column) => (
-                                        <TableCell key={column.id}>{row[column.id]}</TableCell>
+                                        <TableCell key={column.id}>{column.label}</TableCell>
                                     ))}
                                 </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <TablePagination
-                    rowsPerPageOptions={[5, 10, 25]} // Options for rows per page
-                    component="div"
-                    count={sampleData.length} 
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                />
+                            </TableHead>
+                            <TableBody>
+                                {selectedTab === 0 ? (
+                                    prescriberData.map((row) => (
+                                        <TableRow className='table-row' key={row.id}>
+                                            {/* Render prescriber columns */}
+                                            {columns.map((column) => (
+                                                <TableCell key={column.id}>{row[column.id]}</TableCell>
+                                            ))}
+                                        </TableRow>
+                                    ))
+                                ) : (
+                                    patientData.map((row) => (
+                                        <TableRow className='table-row' key={row.id}>
+                                            {/* Render patient columns */}
+                                            {columns.map((column) => (
+                                                <TableCell key={column.id}>{row[column.id]}</TableCell>
+                                            ))}
+                                        </TableRow>
+                                    ))
+                                )}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    {selectedTab === 0 && ( // Show Prescriber Pagination
+                        <TablePagination
+                            rowsPerPageOptions={[5, 10, 25]}
+                            component="div"
+                            count={prescriberData.length} 
+                            rowsPerPage={prescriberRowsPerPage}
+                            page={prescriberPage}
+                            onPageChange={handlePrescriberPageChange}
+                            onRowsPerPageChange={handlePrescriberRowsPerPageChange}
+                        />
+                    )}
+                    {selectedTab === 1 && ( // Show Patient Pagination
+                        <TablePagination
+                            rowsPerPageOptions={[5, 10, 25]}
+                            component="div"
+                            count={patientData.length} 
+                            rowsPerPage={patientRowsPerPage}
+                            page={patientPage}
+                            onPageChange={handlePatientPageChange}
+                            onRowsPerPageChange={handlePatientRowsPerPageChange}
+                        />
+                    )}
+                </div>
             </div>
-        </div>
         </div>
     );
 };
