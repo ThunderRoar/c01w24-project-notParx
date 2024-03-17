398a86db7066f1ac19a756debc09b2f94588df87
Shreyas Rao <raoshreyas2004@gmail.com>
2024-03-17 13:05:44 -0400

    added rest framework object

--- backend/prescriber_service/prescriber_service/settings.py
+++ backend/prescriber_service/prescriber_service/settings.py
@@ -96,6 +96,15 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
