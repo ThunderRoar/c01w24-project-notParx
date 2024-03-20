db5f26ab815c8fcf204aec12ad019e597cf477b0
Cmuss33 <connormusson@gmail.com>
2024-03-20 00:05:16 -0400

    Add routing to different pages

--- frontend/src/components/login/LoginBox.jsx
+++ frontend/src/components/login/LoginBox.jsx
@@ -1,44 +1,137 @@
 import './LoginBox.scss';
 import { Button } from '@mui/material';
 import * as React from 'react';
-import TablePagination from '@mui/material/TablePagination';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import ReactPopup from 'reactjs-popup';
-import axios from 'axios';
+import loginImage from '../../resources/img/LoginText.png';
+import { useNavigate } from 'react-router-dom';
 
 const LoginBox = () => {
+
+    const [activeButton, setActiveButton] = React.useState('Patient');
+
+    const handleButtonClick = (buttonId) => {
+        setActiveButton(buttonId);
+    };
+
+    const getActiveButton = () => {
+        return activeButton
+    }
+
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
+
+    const loginText = loginImage
+    const navigate = useNavigate()
+
+    const loginClicked = async () => {
+
+        try {
+            if (activeButton == "Patient") {
+                const response = await fetch('https://notparx-user-service.azurewebsites.net/api/loginUser/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password })
+                });
+                if (response.ok) {
+                    console.log("Login Success")
+                    navigate('/Patient')
+                } else {
+                  console.error("Login Fail");
+                }
+            } else if (activeButton == "Prescriber") {
+                const response = await fetch('https://notparx-user-service.azurewebsites.net/api/loginPrescriber/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password })
+                });
+                if (response.ok) {
+                    console.log("Login Success")
+                    navigate('/Prescriber')
+                } else {
+                  console.error("Login Fail");
+                }
+            } else {
+                const response = await fetch('https://notparx-user-service.azurewebsites.net/api/loginAdmin/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password })
+                });
+                if (response.ok) {
+                    console.log("Login Success")
+                    navigate('/Admin')
+                } else {
+                  console.error("Login Fail");
+                }
+            }
+          } catch (error) {
+            console.error("Login Error");
+          }
+    }
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    }
+
     return (
-        <div className='login-component'>
-            <div className='content'>
-                <div className='table-container'>
-                    <h2>Log in</h2>
-                    <p>Who are you?</p>
-                    <div className='controls'>                    
-                        <p>Patient</p>
-                        <p>Prescriber</p>
-                        <p>Admin</p>
+        <div className='screen-container'>
+            <div className='image-container'>
+                <img src={loginText} />
+            </div>
+
+            <div className='login-component'>
+                <div className='content'>
+                    <div className='table-container'>
+                        <h2>Log in</h2>
+                        <p>Who are you?</p>
+                        <div className='button-group'>
+                            <button
+                                className={`type-button ${activeButton === 'Patient' ? 'active' : ''}`}
+                                onClick={() => handleButtonClick('Patient')} >
+                                Patient
+                            </button>
+                            <button
+                                className={`type-button ${activeButton === 'Prescriber' ? 'active' : ''}`}
+                                onClick={() => handleButtonClick('Prescriber')} >
+                                Prescriber
+                            </button>
+                            <button
+                                className={`type-button ${activeButton === 'Admin' ? 'active' : ''}`}
+                                onClick={() => handleButtonClick('Admin')} >
+                                Admin
+                            </button>
+                        </div>
+
+                        <p>Username</p>
+                        <input type="text" className="input-field" placeholder="Username" value={username} onChange={handleUsernameChange}/>
+                        <p>Password</p>
+                        <input type="text" className="input-field" placeholder="Password" value={password} onChange={handlePasswordChange}/>
+                        <div className='row'>
+                            <button className='custom-button' onClick={() => loginClicked()}>
+                                <span>Get Started</span>
+                            </button>
+                        </div> 
+                        <div className='row'>
+                            <button className='switch-screen-button'>
+                                <span>Create a new account</span>
+                            </button>
+                        </div> 
+                        
+
+
                     </div>
-
-                    <p>Username</p>
-                    <input type="text" className="input-field" placeholder="Username" />
-                    <p>Password</p>
-                    <input type="text" className="input-field" placeholder="Password" />
-                    <div className='row'>
-                        <button className='custom-button'>
-                            <span>Get Started</span>
-                        </button>
-                    </div> 
-                    
-
-
                 </div>
             </div>
         </div>
+        
     );
 };
 
