23911c42a6c9f69d3bfce0b2bbb8173ab52e0d09
Cmuss33 <connormusson@gmail.com>
2024-03-21 13:57:42 -0400

    Redirect users if already logged in

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
-import { Routes, Route, Outlet } from 'react-router-dom';
-import { adminTabs } from '../shared/tabs-data';
-import './login.scss';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';import './login.scss';
+import decodeToken from '../../token_handling/tokenHandling.js';
 import LoginBox from './LoginBox.jsx';
 import LoginHeader from './LoginHeader.jsx';
 
 const Login = () => {
 
-  const tabs = adminTabs
+  // If user is already logged in, have them get directed to preferred page
+  const navigate = useNavigate()
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedToken = decodeToken(token)
+      if (!decodedToken) {
+      } else if (decodedToken.user_type === 'User'){
+        navigate('/Patient')
+      } else if (decodedToken.user_type === 'Prescriber'){
+        navigate('/Prescriber')
+      } else if (decodedToken.user_type === 'Admin - Coordinator' || decodedToken.user_type === 'Admin - Assistant') {
+        navigate('/Admin')
+      }
+    }
+  });
 
   return (
     <div className="login-component">
