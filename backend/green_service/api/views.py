f9b1897fcb55c54285f529ae552c7671b7528f2d
Shreyas Rao <raoshreyas2004@gmail.com>
2024-03-22 02:35:36 -0400

    added drop down menu to pick green resource

--- backend/green_service/api/views.py
+++ backend/green_service/api/views.py
@@ -13,22 +13,27 @@ from arcgis.gis import GIS
 from dotenv import load_dotenv
 
 load_dotenv()
-ARCGIS_KEY=os.getenv('ARCGIS_KEY')
+GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
 
-# # Create your views here.
-# class MapView(APIView):
-#     permission_classes = [AllowAny]
-
-#     def get(self, request, format=None):
-#         if request.method == "GET":
+# Create your views here.
+class MapView(APIView):
+    permission_classes = [AllowAny]
 
 
+    def get(self, request, format=None):
+        if request.method == "GET":
+            query = request.GET.get('query')
+            api_key = 'GOOGLE_API_KEY'
+            url = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
 
+            params = {
+                'query': query,
+                'key': api_key,
+            }
 
-#             # # Check if the request was successful
-#             # if response.status_code == 200:
-#             #     data = response.json()
-#             # else:
-#             #     return Response(response.text, status=status.HTTP_400_BAD_REQUEST)
-
-#             return JsonResponse(data, safe=False)
+            try:
+                response = requests.get(url, params=params)
+                data = response.json()
+                return JsonResponse(data)
+            except Exception as e:
+                return JsonResponse({'error': str(e)}, status=500)
--- frontend/src/components/greenresources/Greenresources.jsx
+++ frontend/src/components/greenresources/Greenresources.jsx
@@ -1,14 +1,24 @@
 import './Greenresources.scss';
-import "leaflet/dist/leaflet.css"
+import "leaflet/dist/leaflet.css";
+import educationBuilding from '../../resources/img/university_icon.png';
 import React from 'react';
 import { useState, useEffect } from 'react';
+// import { GoogleMap, Marker, InfoWindow, useLoadScript } from '@react-google-maps/api';
+// import { formatRelative } from "date-fns";
 import { MapContainer, TileLayer, useMap, Marker, useMapEvents, Popup } from "react-leaflet";
+import L from "leaflet";
 import axios from 'axios';
 
 
 const GreenResources = () => {
+    // const [mapCenter, setMapCenter] = ([43.7830999, -79.189901]);
+    // const [mapZoom, setMapZoom] = (12);
     const [position, setPosition] = useState(null)
     const [parks, setParks] = useState([]);
+    const [filteredParks, setFilteredParks] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+    const [region, setRegion] = useState('');
+    const [selectedOption, setSelectedOption] = useState('Nature Reserve');
 
     useEffect(() => {
         const fetchData = async () => {
@@ -37,6 +47,67 @@ const GreenResources = () => {
         fetchData();
     }, []);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://overpass-api.de/api/interpreter', {
+                    params: {
+                        data: `[out:json];
+                                area["ISO3166-1"="CA"][admin_level=2];
+                                (
+                                    node["leisure"="${selectedOption.toLowerCase()}"](area);
+                                    way["leisure"="${selectedOption.toLowerCase()}"](area);
+                                    relation["leisure"="${selectedOption.toLowerCase()}"](area);
+                                );
+                                out body;
+                                >;
+                                out skel qt;`,
+                    }
+                });
+                setParks(response.data.elements);
+                console.log(response.data.elements);
+                console.log(selectedOption);
+            } catch (error) {
+                console.error('Error fetching park data:', error);
+            }
+        };
+
+        fetchData();
+    }, [selectedOption]);
+
+    useEffect(() => {
+        const filter = parks.filter(park => {
+            if (region && park.tags.name && park.tags.name.toLowerCase().includes(region.toLowerCase())) {
+                return true;
+            }
+
+            // If user has searched something
+            if (searchQuery) {
+                const searchReq = searchQuery.toLowerCase().split(' ');
+                return searchReq.some(term => park.tags.name.toLowerCase().includes(term));
+            }
+            return true;
+        })
+        setFilteredParks(filter);
+    }, [parks, searchQuery, region]);
+
+    const handleSearchChange = (e) => {
+        setSearchQuery(e.target.value);
+    }
+
+    const handleFilterChange = (e) => {
+        setSelectedOption(e.target.value);
+    }
+
+    const handleRegionChange = (e) => {
+        setRegion(e.target.value);
+    }
+
+    // const handleMapClick = (e) => {
+    //     setPosition(e.latlng);
+    // }
+
+
     function LocationMarker() {
         const map = useMapEvents({
           click() {
@@ -55,6 +126,17 @@ const GreenResources = () => {
         )
     }
 
+    const universityIcon = L.icon({
+        iconUrl: educationBuilding,
+        iconSize: [30, 30],
+        popupAnchor:  [-0, -0],
+    });
+
+    // const parkIcon = L.icon({
+    //     iconUrl: educationBuilding,
+    //     iconSize: [30, 30],
+    //     popupAnchor:  [-0, -0],
+    // });
 
     return (
         <div className='map-component'>
@@ -62,11 +144,18 @@ const GreenResources = () => {
             <div className='content'>
                 <div className='nav'>
                     <div className='map-filter'>
-                        <button>Filter</button>
+                        
+                        <select onChange={handleFilterChange}>
+                            <option>Nature Reserve</option>
+                            <option>Garden</option>
+                            <option>Park</option>
+                            <option>Dog Park</option>
+                        </select>
+
                     </div>
 
                     <div className='map-search-bar'>
-                        <input type="text" placeholder="Search Here..." />
+                        <input type='text' placeholder='Search Here...' value={searchQuery} onChange={handleSearchChange} />
                     </div>
 
                     <div className='map-find-button'>
@@ -74,11 +163,17 @@ const GreenResources = () => {
                     </div>
                 </div>
                 <div className='map-container'>
-                    <MapContainer center={[43.65, -79.39]} zoom={12} scrollWheelZoom={true}>
+                    <MapContainer center={[43.7830999, -79.189901]} zoom={12} scrollWheelZoom={true}>
                         <TileLayer 
                             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                             url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
                         />
+
+                        <Marker position={[43.7830999, -79.189901]} icon={universityIcon}>
+                            <Popup>
+                                UTSC
+                            </Popup>
+                        </Marker>
                         
                         {/* Use the below if user wants to know where they are currenly
                         <LocationMarker/> */}
@@ -96,3 +191,5 @@ const GreenResources = () => {
 };
 
 export default GreenResources;
+
+
