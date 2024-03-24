68f50e81e1dcc2a986d104f88217399e6c2031ae
katarinasotic <64659755+katarinasotic@users.noreply.github.com>
2024-03-24 00:00:28 -0400

    updating routing for patient - green resources

--- frontend/src/components/router/AppRouter.jsx
+++ frontend/src/components/router/AppRouter.jsx
@@ -52,7 +52,7 @@ const router = createBrowserRouter([
             },
             {
               path: "green-resources",
-              element: <Profiles />,
+              element: <GreenResources />,
             },
           ]
         },
