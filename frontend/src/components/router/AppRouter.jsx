fa447a0a57da2f8e0f1167b1835cf8e13ff4a4fd
Shreyas Rao <raoshreyas2004@gmail.com>
2024-03-24 16:17:13 -0400

    router fix

--- frontend/src/components/router/AppRouter.jsx
+++ frontend/src/components/router/AppRouter.jsx
@@ -6,7 +6,7 @@ import Verification from '../admin/verification/Verification';
 import Profiles from '../admin/profiles/Profiles';
 import Admin from '../admin/Admin';
 import GreenResources from '../greenresources/Greenresources';
-import MyPrescriptions from '../prescriber/my-prescriptions/MyPrescriptions';
+// import MyPrescriptions from '../prescriber/my-prescriptions/MyPrescriptions';
 import PrescriberPrescriptions from '../prescriber/my-prescriptions/PrescriberPrescriptions';
 import { Navigate } from 'react-router-dom';
 import PatientPrescriptions from '../patient/my-prescriptions/PatientPrescriptions';
